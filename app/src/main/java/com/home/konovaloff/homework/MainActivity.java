13b9ca66c9bf7b888974dbe7f5a39f1dce7c3930
Denis Konovalov <den.konovaloff@gmail.com>
2019-01-30 15:10:48 +0400

    Добавил главное меню

--- app/src/main/java/com/home/konovaloff/homework/MainActivity.java
+++ app/src/main/java/com/home/konovaloff/homework/MainActivity.java
@@ -2,6 +2,8 @@ package com.home.konovaloff.homework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean res;
+        switch (item.getItemId()){
+            case R.id.menu_add:
+
+                res = true;
+                break;
+            case R.id.menu_clear:
+
+                res = true;
+                break;
+            default:
+                res = super.onOptionsItemSelected(item);
+                break;
+        }
+        return res;
     }
 }
