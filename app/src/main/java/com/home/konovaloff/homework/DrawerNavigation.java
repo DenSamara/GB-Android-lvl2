ad31efe9fb429637450a3dd570b7d5d8edad3986
Denis Konovalov <den.konovaloff@gmail.com>
2019-01-31 11:18:06 +0400

    Добавил NavigationDrawer

---
+++ app/src/main/java/com/home/konovaloff/homework/DrawerNavigation.java
@@ -0,0 +1,51 @@
+package com.home.konovaloff.homework;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public final class DrawerNavigation extends Fragment{
+
+    private NavigationView navigationView;
+    private TextView headerEmployeeId;
+
+    private NavigationView.OnNavigationItemSelectedListener listener;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.app_navigation, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        navigationView = (NavigationView) view.findViewById(R.id.app_navigation);
+        navigationView.setNavigationItemSelectedListener(listener);
+
+        View navigationHeader = navigationView.inflateHeaderView(R.layout.app_navigation_header);
+
+        headerEmployeeId = (TextView) navigationHeader
+                .findViewById(R.id.app_navigation_header_user);
+    }
+
+    public void setNavigationItemSelectedListener(
+            NavigationView.OnNavigationItemSelectedListener listener) {
+        this.listener = listener;
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(listener);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+}
--- app/src/main/java/com/home/konovaloff/homework/MainActivity.java
+++ app/src/main/java/com/home/konovaloff/homework/MainActivity.java
@@ -1,29 +1,94 @@
 package com.home.konovaloff.homework;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Handler;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    boolean doubleBackPress;
-    Handler mHandler;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    public static final String COM_WHATSAPP = "com.whatsapp";
+    private boolean doubleBackPress;
+    private Handler handler;
+
+    private DrawerLayout drawerLayout;
+    private DrawerNavigation navigation;
+    private ActionBarDrawerToggle drawerToggle;
+    private ValueAnimator drawerToggleAnimator;
+    private Toolbar toolbar;
+
+    private final View.OnClickListener navigationClickListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            };
+
+    private final FragmentManager.OnBackStackChangedListener fragmentBackStackListener =
+            new FragmentManager.OnBackStackChangedListener() {
+                @Override
+                public void onBackStackChanged() {
+                    setDrawerToggleEnable(
+                            getSupportFragmentManager().getBackStackEntryCount() == 0);
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportFragmentManager().addOnBackStackChangedListener(fragmentBackStackListener);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        bindContentView(R.layout.activity_main);
 
         this.setTitle(MyApp.getName());
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar ( toolbar );
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
-        mHandler = new Handler();
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
+        drawerToggle.setToolbarNavigationClickListener(navigationClickListener);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        setDrawerToggleEnable(getSupportFragmentManager().getBackStackEntryCount() == 0);
+
+        navigation.setNavigationItemSelectedListener(this);
+
+        handler = new Handler();
+    }
+
+    private void bindContentView(@LayoutRes int layoutResId) {
+        setContentView(layoutResId);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+
+        navigation = (DrawerNavigation) getSupportFragmentManager()
+                .findFragmentById(R.id.drawer_navigation);
     }
 
     @Override
@@ -89,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
         if (!doubleBackPress) {
             doubleBackPress = true;
             Toast.makeText(this, "Для выхода из приложения дважды нажмите 'Назад'", Toast.LENGTH_LONG).show();
-            mHandler.postDelayed(new Runnable() {
+            handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     doubleBackPress = false;
@@ -98,5 +163,135 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    public DrawerLayout getDrawerLayout() {
+        return drawerLayout;
+    }
+
+    private void setDrawerToggleEnable(boolean enable) {
+        if (drawerToggle.isDrawerIndicatorEnabled() == enable) return;
+
+        if (drawerToggleAnimator != null && drawerToggleAnimator.isRunning()) {
+            drawerToggleAnimator.cancel();
+        }
+
+        if (enable) {
+            drawerToggle.getDrawerArrowDrawable().setProgress(1f);
+            drawerToggle.setDrawerIndicatorEnabled(true);
+
+            drawerToggleAnimator = ValueAnimator.ofFloat(1f, 0f);
+        } else {
+            drawerToggleAnimator = ValueAnimator.ofFloat(0f, 1f);
+            drawerToggleAnimator.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    drawerToggle.setDrawerIndicatorEnabled(false);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+        }
+
+        drawerToggleAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                drawerToggle.getDrawerArrowDrawable()
+                        .setProgress((float) animation.getAnimatedValue());
+            }
+        });
+
+        drawerToggleAnimator.setInterpolator(new DecelerateInterpolator());
+        drawerToggleAnimator.setDuration(300);
+        drawerToggleAnimator.start();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        switch (item.getItemId()) {
+            case R.id.app_navigation_action_sync:
+                startSynchronization();
+                break;
+            case R.id.app_navigation_whatsapp:
+                startActionWhatsApp();
+                return true;
+            case R.id.app_navigation_about:
+                startActionAbout();
+                return true;
+            case R.id.app_navigation_downloads_update:
+                final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+                break;
+            case R.id.app_navigation_sign_out:
+                onSignOut();
+                break;
+        }
+        return false;
+    }
+
+    private void startActionWhatsApp() {
+        try {
+            PackageManager pm = getPackageManager();
+            PackageInfo info = pm.getPackageInfo(COM_WHATSAPP, PackageManager.GET_META_DATA);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Global.toast("WhatsApp не установлен");
+            return;
+        }
+
+        //TODO Имя пользователя
+        String fio = "";
+
+        String description;
+        if (fio != null && !fio.isEmpty()) {
+            description = String.format("Здравствуйте! Меня зовут %s. У меня есть вопрос пр приложению %s:\n", fio, MyApp.getName());
+        } else
+            description = String.format("Здравствуйте! У меня есть вопрос пр приложению %s:\n", MyApp.getName());
+
+        Intent sendIntent = new Intent("android.intent.action.MAIN");
+//        sendIntent.putExtra("jid", number + "@s.whatsapp.net"); // Для получения текущего номера нужны дополнительные привилегии
+        sendIntent.putExtra(Intent.EXTRA_TEXT, description);
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setPackage(COM_WHATSAPP);
+        sendIntent.setType("text/plain");
+
+        startActivity(sendIntent);
+    }
+
+    private void startActionAbout() {
+        Global.toast(String.format("Автор: Коновалов Денис. Проект выполнен в качестве домашнего задания в рамках обучения программированию для платформы Android"));
+    }
+
+    /**
+     * Запускаем процесс обновления данных
+     */
+    private void startSynchronization() {
+
+    }
+
+    /**
+     * Меняем логин и лого на дефолт
+     */
+    private void onSignOut() {
+
     }
 }
